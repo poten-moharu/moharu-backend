9078aa60ff9d699c02cd944b940d6f8724e6bb5b
ParkJJungmIn <v5835613v@naver.com>
2024-05-24 23:39:04 +0900

    feat: 액티비티 API 업데이트

---
+++ src/auth/guards/jwt-optional-auth.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class OptionalAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    if (err || !user) {
+      return null;
+    }
+    return user;
+  }
+}
